7ff43b9b9241b662e74d60dddddc49ad53bbac35
John <user@axewhy.local>
2023-01-11 09:26:27 +0530

    added: foxbeta logo in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@components/Container";
 import { Logo } from "@components/Logo";
+import Image from "next/image";
+import foxbetaLogoLight from "@images/foxbeta-w.png";
 
 export function Footer() {
   return (
@@ -11,6 +13,22 @@ export function Footer() {
           <span className='font-semibold'>Foxbeta Pvt Ltd.</span> All rights
           reserved.
         </p>
+      </Container>
+      <Container className='flex items-center justify-center mt-8'>
+        <div className='space-y-3 text-center text-slate-300'>
+          <h5 className='text-xs font-semibold tracking-widest uppercase'>
+            Powered by
+          </h5>
+          <div>
+            <Image
+              alt='Foxbeta Pvt Ltd'
+              src={foxbetaLogoLight}
+              className='w-auto h-16 md:h-18 drop-shadow-3xl'
+              unoptimized
+              priority
+            />
+          </div>
+        </div>
       </Container>
     </footer>
   );
